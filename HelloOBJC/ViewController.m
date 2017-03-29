c06ca1161883009944a8d27c58083100829abbab
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-03-29 15:38:18 +0100

    added ask a question button with views and alert to show correct msg or not

--- HelloOBJC/ViewController.m
+++ HelloOBJC/ViewController.m
@@ -25,12 +25,27 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)clickBait:(id)sender {
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Question" message:@"2 + 2 * 2 = 8?" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *yes = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        UIAlertController *result = [UIAlertController alertControllerWithTitle:@"Answer" message:@"That is incorrect" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Done" style:UIAlertActionStyleCancel handler:nil];
+        [result addAction:ok];
+        [self presentViewController:result animated:YES completion:nil];
+        
+        
+    }];
     
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"WELCOME" message:@"Welcome to the world of Objective-C" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *closeAction = [UIAlertAction actionWithTitle:@"CLOSE" style:UIAlertActionStyleCancel handler:nil];
-    [alert addAction:closeAction];
+    UIAlertAction *no = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        UIAlertController *result = [UIAlertController alertControllerWithTitle:@"Answer" message:@"That is correct!" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Done" style:UIAlertActionStyleCancel handler:nil];
+        [result addAction:ok];
+        [self presentViewController:result animated:YES completion:nil];
+    }];
+    [alert addAction:yes];
+    [alert addAction:no];
     [self presentViewController:alert animated:YES completion:nil];
 }
-
 
 @end
