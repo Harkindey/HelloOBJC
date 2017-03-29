2deb91f73d0c11d46e22289deda17239a9b9ef9f
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-03-29 15:13:01 +0100

    give a click action in Objective C

---
+++ HelloOBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloOBJC
+//
+//  Created by Emmanuel Akinde on 3/29/17.
+//  Copyright © 2017 Emmanuel Akinde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
